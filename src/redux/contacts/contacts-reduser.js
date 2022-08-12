67ba84b18ec2ed61aa5ceff6b0bcb47e5040f7ed
IlliaBekarevych <93090373+IlliaBekarevych@users.noreply.github.com>
2022-08-12 18:05:23 +0300

    chenge localstorage

--- src/redux/contacts/contacts-reduser.js
+++ src/redux/contacts/contacts-reduser.js
@@ -2,7 +2,7 @@ import { createReducer } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import { addContact, deleteContact, chengeFilter } from './contact-actions';
 
-const contactList = JSON.parse(window.localStorage.getItem('contacts')) ?? [
+const contactList = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
